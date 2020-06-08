f6deaf57e17e127ff6aaf39ee28817af9588d006
TwinkleGhatoriya <twinkle.ghatoriya@gmail.com>
2020-06-08 19:32:49 -0400

    Hello World homework

---
+++ src/com/twinkle/java/Addition.java
@@ -0,0 +1,7 @@
+package com.twinkle.java;
+
+public class Addition {
+	public static void main (String [] arg) { 
+		System.out.println(2+2);
+		}
+}
---
+++ src/com/twinkle/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.twinkle.java;
+
+public class HelloWorld {
+	
+	public static void main (String [] arg) {
+		System.out.print("Hello World");
+	}
+	
+}
