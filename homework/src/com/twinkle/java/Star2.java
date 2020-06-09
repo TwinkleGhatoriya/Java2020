71ce94b672dadb16111c6198b1e85be2a5ff2d30
TwinkleGhatoriya <twinkle.ghatoriya@gmail.com>
2020-06-09 18:14:37 -0400

    Folder change

--- homework/src/com/twinkle/java/Star2.java
+++ homework/src/com/twinkle/java/Star2.java
@@ -3,8 +3,8 @@ package com.twinkle.java;
 public class Star2 {
 	
 	public static void main (String [] arg) { 
-		System.out.println("    *  ");
-		System.out.println("   * * ");
+		System.out.println("    *");
+		System.out.println("   * *");
 		System.out.println("  * * *");
 		System.out.println(" * * * *");
 		System.out.println("* * * * *");
