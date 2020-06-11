6c6518e70cfcad87c511fcc253926395c4a2b70a
TwinkleGhatoriya <twinkle.ghatoriya@gmail.com>
2020-06-11 18:35:08 -0400

    Homework2.3

---
+++ day2/src/day2/PrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class PrimitiveDataTypes {
+	public static void main (String [] args) {
+		byte a = 5;
+		System.out.println(a);
+		
+		short b = 10;
+		System.out.println(b);
+		
+		int c = 125;
+		System.out.println(c);
+		
+		long d = 205;
+		System.out.println(d);
+		
+		float e = 125;
+		System.out.println(e);
+		
+		double f = 2555.7;
+		System.out.println(f);
+		
+		boolean g = false;
+		System.out.println(g);
+		
+		char h = 't';
+		System.out.println(h);
+		
+	}
+
+}
